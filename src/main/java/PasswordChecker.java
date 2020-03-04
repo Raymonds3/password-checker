a70428b827fed2871dc1ae9f18a09d176748a937
Raymonds3 <raymonds338@gmail.com>
2020-03-04 14:22:56 +0200

    Exception update

--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PasswordChecker {
     public static String uppercase = "(.*[A-Z].*)";
     public static String lowercase = "(.*[a-z].*)";
@@ -5,75 +7,92 @@ public class PasswordChecker {
     public static String specialChar = "(.*[! # @ $ % ^ & * ( ) - _ = + [ ] ; : ' \" , < . > / ?].*)";
     public static String space = "(.*[ ].*)";
 
-    public static boolean passwordIsValid(String password) {
-        if(password.isEmpty()) {
-            System.out.println("- password should exist");
-            return false;
-        }else{
+    public static String passwordIsValid(String password) throws Exception{
+        String result = "";
+        if (password.isEmpty()) {
+            result = "- password should exist";
+            throw new Exception("- password should exist");
+        } else {
             if (password.length() < 8) {
-                System.out.println("- password should be longer than than 8 characters");
-                return false;
-            }else{
-                if(!password.matches(lowercase)) {
-                    System.out.println("- password should have at least one lowercase letter");
-                    return false;
-                }else {
-                    if(!password.matches(uppercase)){
-                        System.out.println("- password should have at least one uppercase letter");
-                        return false;
-                    }else {
-                        if(!password.matches(digit)){
-                            System.out.println("- password should at least have one digit");
-                            return false;
-                        }else{
-                            if(!password.matches(specialChar)){
-                                System.out.println("- password should have at least one special character");
-                                return false;
-                            }else {
-                                if(password.matches(space)){
-                                    System.out.println("- password shouldn't have spaces");
-                                    return false;
-                                }else {
+                result = "- password should be longer than than 8 characters";
+                throw new Exception("- password should be longer than than 8 characters");
+            } else {
+                if (!password.matches(lowercase)) {
+                    result = "- password should have at least one lowercase letter";
+                    throw new Exception("- password should have at least one lowercase letter");
+                } else {
+                    if (!password.matches(uppercase)) {
+                        result = "- password should have at least one uppercase letter";
+                        throw new Exception("- password should have at least one uppercase letter");
+                    } else {
+                        if (!password.matches(digit)) {
+                            result = "- password should at least have one digit";
+                            throw new Exception("- password should at least have one digit");
+                        } else {
+                            if (!password.matches(specialChar)) {
+                                result = "- password should have at least one special character";
+                                throw new Exception("- password should have at least one special character");
+                            } else {
+                                if (password.matches(space)) {
+                                    result = "- password shouldn't have spaces";
+                                    throw new Exception("- password shouldn't have spaces");
+                                } else {
+                                    result = "Password is Valid";
                                     System.out.println("Password is Valid");
-                                    return true;
                                 }
                             }
                         }
-
                     }
                 }
             }
-
         }
+        return result;
     }
 
-    public static boolean passwordIsOk(String password){
+    public static String passwordIsOk(String password) throws Exception{
+        String result1 = "";
         if(password.isEmpty()){
-            System.out.println("the password is never OK if it doesn't exist and its" +
+            result1 = "the password is never OK if it doesn't exist and its" +
+                    " less than 8 characters are not met.";
+            throw new Exception("the password is never OK if it doesn't exist and its" +
                     " less than 8 characters are not met.");
-            return false;
         }
         if(password.length() < 8){
-            System.out.println("the password is never OK if it doesn't exist and its" +
+            result1 = "the password is never OK if it doesn't exist and its" +
+                    " less than 8 characters are not met.";
+            throw new Exception("the password is never OK if it doesn't exist and its" +
                     " less than 8 characters are not met.");
-            return false;
         } else {
             if (!password.matches(lowercase) || !password.matches(uppercase) ||
             !password.matches(digit) || !password.matches(specialChar)){
-                System.out.println("password is OK");
-                return false;
+                result1 = "password is OK";
+                throw new Exception("password is OK");
             }else {
+                result1 = "Password is Valid";
                 System.out.println("Password is Valid");
-                return true;
             }
         }
-
+        return result1;
     }
 
 
     public static void main(String[] args) {
+        String password1;
+        Scanner passwordInput = new Scanner(System.in);
+        System.out.println("Please Enter Password?");
+        password1 = passwordInput.nextLine();
 
-        passwordIsValid("raymondsere");
-        passwordIsOk("raymondsere");
+
+        try {
+            passwordIsValid(password1);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        try {
+            passwordIsOk(password1);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- src/test/java/PasswordCheckerTest.java
+++ src/test/java/PasswordCheckerTest.java
@@ -1,23 +1,58 @@
 import org.junit.Assert;
+import org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PasswordCheckerTest {
     @Test
-    public void GlobalVariables(){
-        Assert.assertEquals(PasswordChecker.uppercase,"(.*[A-Z].*)");
-        Assert.assertEquals(PasswordChecker.lowercase,"(.*[a-z].*)");
-        Assert.assertEquals(PasswordChecker.digit,"(.*[0-9].*)");
-        Assert.assertEquals(PasswordChecker.specialChar,"(.*[! # @ $ % ^ & * ( ) - _ = + [ ] ; : ' \" , < . > / ?].*)");
-        Assert.assertEquals(PasswordChecker.space,"(.*[ ].*)");
+    public void GlobalVariablesTest(){
+        assertEquals(PasswordChecker.uppercase,"(.*[A-Z].*)");
+        assertEquals(PasswordChecker.lowercase,"(.*[a-z].*)");
+        assertEquals(PasswordChecker.digit,"(.*[0-9].*)");
+        assertEquals(PasswordChecker.specialChar,"(.*[! # @ $ % ^ & * ( ) - _ = + [ ] ; : ' \" , < . > / ?].*)");
+        assertEquals(PasswordChecker.space,"(.*[ ].*)");
     }
-    @Test
-    public void passwordIsValid(){
-        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds3@"),true);
-        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds3"),false);
+    @Test(expected = Exception.class)
+    public void passwordIsValidTest() throws Exception {
+
+        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds@3"),"Password is Valid");
+
+        String expectedexist = "- password should exist";
+        Assert.assertEquals(PasswordChecker.passwordIsValid(""), expectedexist);
+
+        String expected = "- password should be longer than than 8 characters";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("ray"), expected);
+
+        String expectedlower = "- password should have at least one lowercase letter";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("RAYMONDS@3"), expectedlower);
+
+        String expectedupper = "- password should have at least one uppercase letter";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("raymonds@3"), expectedupper);
+
+        String expecteddigit = "- password should at least have one digit";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds@"), expecteddigit);
+
+        String expectedspecial = "- password should have at least one special character";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds3"), expectedspecial);
+
+        String expectedspace = "- password shouldn't have spaces";
+        Assert.assertEquals(PasswordChecker.passwordIsValid("Raymonds@ 3"), expectedspace);
     }
-    @Test
-    public void passwordIsOk(){
-        Assert.assertEquals(PasswordChecker.passwordIsOk("Raymond3@"),true);
-        Assert.assertEquals(PasswordChecker.passwordIsOk("Raymond3"),false);
+
+    @Test(expected = Exception.class)
+    public void passwordIsOkTest() throws Exception {
+        Assert.assertEquals(PasswordChecker.passwordIsOk("Raymonds@3"),"Password is Valid");
+
+        String expected = "the password is never OK if it doesn't exist and its" +
+                " less than 8 characters are not met.";
+        Assert.assertEquals(PasswordChecker.passwordIsOk(""), expected);
+
+        String expected1 = "the password is never OK if it doesn't exist and its" +
+                " less than 8 characters are not met.";
+        Assert.assertEquals(PasswordChecker.passwordIsOk("ray"), expected1);
+
+        String expectedok = "password is OK";
+        Assert.assertEquals(PasswordChecker.passwordIsOk("Raymonds3"), expectedok);
     }
 }
